807b8a4b92a0c9fbc84db30d014a10d45cedb0d5
Kazi Fahim Imam <kazi.fahim@pathao.com>
2024-03-02 12:16:10 +0600

    Added more redis functionalities

--- socketHandler.js
+++ socketHandler.js
@@ -1,8 +1,6 @@
 
-const {
-  saveMessageToRedis, getRecentMessages, setUserActive, getUserActive,
-} = require('./utils/redisUtils');
-const {saveMessage} = require('./src/repos/chatRoom');
+const { getRecentMessages, setUserOnlineStatus, AddOnlineUsers, DeleteOnlineUsers } = require('./utils/redisUtils');
+const { saveMessage } = require('./src/repos/chatRoom');
 const { verifyToken } = require('./utils/jwtUtils');
 
 // socketHandler.js
@@ -16,11 +14,16 @@ const socketHandler = (io) => {
       }
       console.log("Connected user : ", userMail)
 
-      
-      socket.on('joinRoom', (roomId) => {
+      // * JoinRoom - Logics
+      socket.on('joinRoom', async (roomId) => {
         console.log("joined Room ---------", roomId);
         socket.join(roomId);
 
+        // ? Get the 10 latest messages of the room that user is joining
+        let count = 10
+        const messages = await getRecentMessages(roomId, count);
+        //TODO: Discuss about this, will all the members see messages again?
+        socket.emit('receiveMessage', messages);
         socket.broadcast
         .to(roomId)
         .emit(
@@ -28,9 +31,13 @@ const socketHandler = (io) => {
           `${userMail} joined the room`,
         );
         socket.emit('receiveMessage', `Welcome to room no ${roomId}`);
-        setUserActive({roomId, userMail, status:true})
+
+        await AddOnlineUsers(roomId, userMail).then( () => {
+          console.log("Added user to the room with online status")
+        })
       });
-  
+
+      // * SendMessage Logics
       socket.on('sendMessage', (data) => {
         const { roomId, message } = data;
         (async () => {
@@ -43,7 +50,8 @@ const socketHandler = (io) => {
         })();
         io.to(roomId).emit('receiveMessage', message);
       });
-  
+
+      // * ReceiveMessage Logics
       socket.on('receiveMessage', (message) => {
         // Handle received message, e.g., log it or perform custom actions
         console.log('---------Received message: ', message);
@@ -53,15 +61,20 @@ const socketHandler = (io) => {
         // io.to(roomId).emit('receiveMessage', message);
       });
 
+      // * GetRecentMessages Logics
       socket.on('getRecentMessages', async (data) => {
         const { roomId, count } = data;
         const messages = await getRecentMessages(roomId, count);
         socket.emit('recentMessages', messages);
       });
-  
-      socket.on('disconnect', () => {
-        // TODO Think About How to Handle this logic
-        // setUserActive({roomId, userMail, status:false})
+
+      // * disconnect Logics
+      socket.on('disconnect', (roomId, userMail) => {
+        DeleteOnlineUsers(roomId, userMail).then(r => {
+          console.log("deleted users")
+        }).catch(err => {
+          console.log("Error occurred ", err)
+        })
         console.log('User disconnected');
       });
     });
--- utils/redisUtils.js
+++ utils/redisUtils.js
@@ -1,5 +1,6 @@
 // src/utils/redisUtils.js
 const redis = require('redis');
+const math = require('math')
 
 const client = redis.createClient();
 
@@ -54,16 +55,91 @@ async function cacheRecentMessages(roomId, messages) {
 
 async function getRecentMessages(roomId, count) {
   const key = `room_id_${roomId}`;
-  const messages = await client.lRange(key, 0, count-1);
+  const listLength = await client.lLen(key);
+  let limit = math.min(listLength, count-1)
+  const messages = await client.lRange(key, 0, limit);
   return messages.map((message) => JSON.parse(message));
 }
 
-const setUserActive = ({roomId, userMail, status}) => {
+const setUserOnlineStatus = async ({roomId, userMail, online}) => {
+  key = `${roomId}_${userMail}`
   // set to redis, make active/delete
+    await client.exists(key, async (err, reply) => {
+      if (reply === 1) { // Key is in redis
+        if (online) { // status should be online
+          console.log('exists');
+        } else { // status should be offline, so we delete the key
+          await client.del(key, function(err, response) {
+            if (response === 1) {
+              console.log("Deleted Successfully!")
+            } else{
+              console.log("Cannot delete")
+            }
+          })
+        }
+      } else { // Key Is not in redis
+        if (online) { // status should be online
+          await client.set(key, function(err, response) {
+            if (response === 1) {
+              console.log("set redis data successfully")
+            } else{
+              console.log("Cannot set redis data")
+            }
+          })
+        } else {
+          console.log('user is not online')
+        }
+      }
+    });
 }
 
-const getUserActive = (roomId) => {
-  // fetch active list of users from redis
+const getOnlineUsers = async (roomId) => {
+  let key = `${roomId}_online_members`
+  try {
+    // Push the new member to the online users set
+    const onlineMembers = await client.sMembers(key);
+    if (onlineMembers.length === 0) {
+      console.log("no online members")
+    } else {
+      console.log("retrieved online members")
+      return onlineMembers
+    }
+
+  } catch (error) {
+    console.error("Error saving message to Redis:", error);
+  }
 }
 
-module.exports = { saveMessageToRedis, getRecentMessages, cacheRecentMessages, getUserActive, setUserActive };
+const AddOnlineUsers = async (roomId, memberEmail) => {
+  let key = `${roomId}_online_members`
+  try {
+    // Push the new member to the online users set
+    const res1 = await client.sAdd(key, memberEmail);
+    if (res1 === 0) {
+      console.log("Could not set data to redis")
+    } else {
+      console.log("Set data to redis under key ", key)
+    }
+
+  } catch (error) {
+    console.error("Error saving message to Redis:", error);
+  }
+}
+
+const DeleteOnlineUsers = async (roomId, memberEmail) => {
+  let key = `${roomId}_online_members`
+  try {
+    // Push the new member to the online users set
+    const res1 = await client.sRem(key, memberEmail);
+    if (res1 === 0) {
+      console.log("Could not delete data from redis")
+    } else {
+      console.log("removed data from redis ", key)
+    }
+
+  } catch (error) {
+    console.error("Error saving message to Redis:", error);
+  }
+}
+
+module.exports = { saveMessageToRedis, getRecentMessages, cacheRecentMessages, getOnlineUsers, setUserOnlineStatus, AddOnlineUsers, DeleteOnlineUsers };
