eb03c460a57d120b395b23a6f7760ffcd662a058
fardinabir <fardinabir@gmail.com>
2024-03-03 21:03:40 +0600

    edited API path and dashboard css

--- public/js/authLogin.js
+++ public/js/authLogin.js
@@ -11,7 +11,7 @@ loginButton.addEventListener("click", (e) => {
       "<p>Both email and password are required!</p>";
   } else {
     axios
-      .post("/auth/login", {
+      .post("/api/login", {
         email,
         password,
       })
